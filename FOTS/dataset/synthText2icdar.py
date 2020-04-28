9d63d15971b0a98b303a52f1a7928783b2afccbd
Masao-Taketani <taket003@umn.edu>
2020-04-28 23:56:27 +0900

    modified the conversion program.

--- FOTS/dataset/synthText2icdar.py
+++ FOTS/dataset/synthText2icdar.py
@@ -89,8 +89,9 @@ def Mat2icdar(matfile, saveFolder):
     for i in range(len(data['txt'][0])):
         contents = []
         for val in data['txt'][0][i]:
-            v = [x.split("\n") for x in val.strip().split(" ")]
-            contents.extend(sum(v, []))
+            v = [x.strip() for x in val.strip().split("\n")]
+            #contents.extend(sum(v, []))
+            contents.extend(v)
         error_msg = "No.{} data".format(i)
         print(error_msg, file=sys.stderr)
         rec = np.array(data['wordBB'][0][i], dtype=np.int32)
