2acd1f960d78fa76fd3207daa337d64a1b6b7441
Masao-Taketani <taket003@umn.edu>
2020-06-15 22:45:26 +0900

    cleaned the code

--- module/Backbone_branch.py
+++ module/Backbone_branch.py
@@ -10,7 +10,9 @@ from nets import resnet_v1
 FLAGS = tf.app.flags.FLAGS
 
 def unpool(inputs):
-    return tf.image.resize_bilinear(inputs, size=[tf.shape(inputs)[1]*2,  tf.shape(inputs)[2]*2])
+    return tf.image.resize_bilinear(inputs,
+                                    size=[tf.shape(inputs)[1]*2,
+                                    tf.shape(inputs)[2]*2])
 
 def mean_image_subtraction(images, means=[123.68, 116.78, 103.94]):
     '''
@@ -38,7 +40,9 @@ class Backbone(object):
         images = mean_image_subtraction(images)
 
         with slim.arg_scope(resnet_v1.resnet_arg_scope(weight_decay=weight_decay)):
-            logits, end_points = resnet_v1.resnet_v1_50(images, is_training=self.is_training, scope='resnet_v1_50')
+            logits, end_points = resnet_v1.resnet_v1_50(images,
+                                                        is_training=self.is_training,
+                                                        scope='resnet_v1_50')
 
         with tf.variable_scope('feature_fusion', values=[end_points.values]):
             batch_norm_params = {
@@ -63,28 +67,47 @@ class Backbone(object):
                     if i == 0:
                         h[i] = f[i]
                     else:
-                        c1_1 = slim.conv2d(tf.concat([g[i-1], f[i]], axis=-1), num_outputs[i], 1)
+                        c1_1 = slim.conv2d(tf.concat([g[i-1],
+                                                      f[i]],
+                                                     axis=-1),
+                                           num_outputs[i],
+                                           1)
                         h[i] = slim.conv2d(c1_1, num_outputs[i], 3)
                     if i <= 2:
                         g[i] = unpool(h[i])
                     else:
                         g[i] = slim.conv2d(h[i], num_outputs[i], 3)
-                    print('Shape of h_{} {}, g_{} {}'.format(i, h[i].shape, i, g[i].shape))
+                    print('Shape of h_{} {}, g_{} {}'.format(i,
+                                                             h[i].shape,
+                                                             i,
+                                                             g[i].shape))
 
                 # here we use a slightly different way for regression part,
                 # we first use a sigmoid to limit the regression range, and also
                 # this is do with the angle map
-                F_score = slim.conv2d(g[3], 1, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None)
+                F_score = slim.conv2d(g[3],
+                                      1,
+                                      1,
+                                      activation_fn=tf.nn.sigmoid,
+                                      normalizer_fn=None)
                 # 4 channel of axis aligned bbox and 1 channel rotation angle
-                geo_map = slim.conv2d(g[3], 4, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None) * FLAGS.text_scale
-                angle_map = (slim.conv2d(g[3], 1, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None) - 0.5) * np.pi/2 # angle is between [-45, 45]
+                geo_map = slim.conv2d(g[3],
+                                      4,
+                                      1,
+                                      activation_fn=tf.nn.sigmoid,
+                                      normalizer_fn=None) * FLAGS.text_scale
+                angle_map = (slim.conv2d(g[3],
+                                         1,
+                                         1,
+                                         activation_fn=tf.nn.sigmoid,
+                                         normalizer_fn=None)
+                             - 0.5) * np.pi/2 # angle is between [-45, 45]
                 F_geometry = tf.concat([geo_map, angle_map], axis=-1)
 
         return g[3], F_score, F_geometry
 
 
-    def dice_coefficient(self, y_true_cls, y_pred_cls,
-                         training_mask):
+    def dice_coefficient(self, y_true_cls, y_pred_cls, training_mask):
         '''
         dice loss
         :param y_true_cls:
@@ -101,9 +124,7 @@ class Backbone(object):
 
 
 
-    def loss(self, y_true_cls, y_pred_cls,
-             y_true_geo, y_pred_geo,
-             training_mask):
+    def loss(self, y_true_cls, y_pred_cls, y_true_geo, y_pred_geo, training_mask):
         '''
         define the loss used for training, contraning two part,
         the first part we use dice loss instead of weighted logloss,
@@ -120,8 +141,12 @@ class Backbone(object):
         classification_loss *= 0.01
 
         # d1 -> top, d2->right, d3->bottom, d4->left
-        d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
-        d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
+        d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo,
+                                                        num_or_size_splits=5,
+                                                        axis=3)
+        d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo,
+                                                                  num_or_size_splits=5,
+                                                                  axis=3)
         area_gt = (d1_gt + d3_gt) * (d2_gt + d4_gt)
         area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
         w_union = tf.minimum(d2_gt, d2_pred) + tf.minimum(d4_gt, d4_pred)
@@ -130,8 +155,10 @@ class Backbone(object):
         area_union = area_gt + area_pred - area_intersect
         L_AABB = -tf.log((area_intersect + 1.0)/(area_union + 1.0))
         L_theta = 1 - tf.cos(theta_pred - theta_gt)
-        tf.summary.scalar('geometry_AABB', tf.reduce_mean(L_AABB * y_true_cls * training_mask))
-        tf.summary.scalar('geometry_theta', tf.reduce_mean(L_theta * y_true_cls * training_mask))
+        tf.summary.scalar('geometry_AABB',
+                          tf.reduce_mean(L_AABB * y_true_cls * training_mask))
+        tf.summary.scalar('geometry_theta',
+                          tf.reduce_mean(L_theta * y_true_cls * training_mask))
         L_g = L_AABB + 20 * L_theta
 
         return tf.reduce_mean(L_g * y_true_cls * training_mask) + classification_loss
--- module/RoI_rotate.py
+++ module/RoI_rotate.py
@@ -14,7 +14,12 @@ class RoIRotate(object):
 	def __init__(self, height=8):
 		self.height = height
 
-	def roi_rotate_tensor(self, feature_map, transform_matrixs, box_masks, box_widths, is_debug=False):
+	def roi_rotate_tensor(self,
+						  feature_map,
+						  transform_matrixs,
+						  box_masks,
+						  box_widths,
+						  is_debug=False):
 		"""
 		param:
 		feature_map: N * H * W * C
@@ -23,7 +28,9 @@ class RoIRotate(object):
 		box_widths: N'
 		"""
 		with tf.variable_scope("RoIrotate"):
-			max_width = box_widths[tf.argmax(box_widths, 0, output_type=tf.int32)]
+			max_width = box_widths[tf.argmax(box_widths,
+											 0,
+											 output_type=tf.int32)]
 			box_widths = tf.cast(box_widths, tf.float32)
 			tile_feature_maps = []
 			# crop_boxes = []
@@ -32,7 +39,8 @@ class RoIRotate(object):
 			map_shape = tf.shape(feature_map)
 			map_shape = tf.cast(map_shape, tf.float32)
 
-			for i, mask in enumerate(box_masks): # box_masks is a list of num of rois in each feature map
+			# box_masks is a list of num of rois in each feature map
+			for i, mask in enumerate(box_masks):
 				_feature_map = feature_map[i]
 				# _crop_box = tf.constant([0, 0, 8/map_shape[0], box_widths[i]/map_shape[1]])
 				# _crop_size = tf.constant([8, tf.cast(box_widths[i], tf.int32)])
@@ -49,7 +57,10 @@ class RoIRotate(object):
 			ones = tf.ones_like(norm_box_widths)
 			norm_box_heights = ones * (8.0 / map_shape[1])
 			zeros = tf.zeros_like(norm_box_widths)
-			crop_boxes = tf.transpose(tf.stack([zeros, zeros, norm_box_heights, norm_box_widths]))
+			crop_boxes = tf.transpose(tf.stack([zeros,
+												zeros,
+												norm_box_heights,
+												norm_box_widths]))
 			"""
 			box_height = ones * 8
 			box_height = tf.cast(box_height, tf.int32)
@@ -60,11 +71,15 @@ class RoIRotate(object):
 			# crop_boxes = tf.stack(crop_boxes, axis=0)
 			# crop_sizes = tf.stack(crop_sizes, axis=0)
 
-			trans_feature_map = transformer(tile_feature_maps, transform_matrixs)
+			trans_feature_map = transformer(tile_feature_maps,
+											transform_matrixs)
 
 			# box_inds = tf.concat(box_masks, axis=0)
 			box_inds = tf.range(tf.shape(trans_feature_map)[0])
-			rois = tf.image.crop_and_resize(trans_feature_map, crop_boxes, box_inds, crop_size)
+			rois = tf.image.crop_and_resize(trans_feature_map,
+											crop_boxes,
+											box_inds,
+											crop_size)
 
 			pad_rois = tf.image.pad_to_bounding_box(rois, 0, 0, 8, max_width)
 
@@ -72,9 +87,15 @@ class RoIRotate(object):
 
 			return pad_rois
 
-	def roi_rotate_tensor_pad(self, feature_map, transform_matrixs, box_masks, box_widths):
+	def roi_rotate_tensor_pad(self,
+							  feature_map,
+							  transform_matrixs,
+							  box_masks,
+							  box_widths):
 		with tf.variable_scope("RoIrotate"):
-			max_width = box_widths[tf.argmax(box_widths, 0, output_type=tf.int32)]
+			max_width = box_widths[tf.argmax(box_widths,
+											 0,
+											 output_type=tf.int32)]
 			# box_widths = tf.cast(box_widths, tf.float32)
 			tile_feature_maps = []
 			# crop_boxes = []
@@ -95,8 +116,10 @@ class RoIRotate(object):
 				tile_feature_maps.append(_feature_map)
 				# box_inds.append(i)
 
-			tile_feature_maps = tf.concat(tile_feature_maps, axis=0) # N' * H * W * C where N' = N * B
-			trans_feature_map = transformer(tile_feature_maps, transform_matrixs)
+			tile_feature_maps = tf.concat(tile_feature_maps,
+										  axis=0) # N' * H * W * C where N' = N * B
+			trans_feature_map = transformer(tile_feature_maps,
+											transform_matrixs)
 
 			box_nums = tf.shape(box_widths)[0]
 			pad_rois = tf.TensorArray(tf.float32, box_nums)
@@ -108,8 +131,19 @@ class RoIRotate(object):
 				_affine_feature_map = trans_feature_map[i]
 				width_box = box_widths[i]
 				# _affine_feature_map = tf.expand_dims(_affine_feature_map, 0)
-				# roi = tf.image.crop_and_resize(after_transform, [[0, 0, 8/map_shape[0], width_box/map_shape[1]]], [0], [8, tf.cast(width_box, tf.int32)])
-				roi = tf.image.crop_to_bounding_box(_affine_feature_map, 0, 0, 8, width_box)
+				# roi = tf.image.crop_and_resize(after_transform,
+				#								 [[0,
+				#								   0,
+				#								   8/map_shape[0],
+				#								   width_box/map_shape[1]]],
+				#								 [0],
+				#								 [8,
+				#								  tf.cast(width_box, tf.int32)])
+				roi = tf.image.crop_to_bounding_box(_affine_feature_map,
+													0,
+													0,
+													8,
+													width_box)
 				pad_roi = tf.image.pad_to_bounding_box(roi, 0, 0, 8, max_width)
 				pad_rois = pad_rois.write(i, pad_roi)
 				i += 1
@@ -122,7 +156,12 @@ class RoIRotate(object):
 
 			return pad_rois
 
-	def roi_rotate_tensor_while(self, feature_map, transform_matrixs, box_masks, box_widths, is_debug=False):
+	def roi_rotate_tensor_while(self,
+								feature_map,
+								transform_matrixs,
+								box_masks,
+								box_widths,
+								is_debug=False):
 		assert transform_matrixs.shape[-1] != 8
 		"""
 		Input:
@@ -154,17 +193,54 @@ class RoIRotate(object):
 				width_box = tf.cast(width_box, tf.float32)
 
 				# Project transform
-				after_transform = tf.contrib.image.transform(_feature_map, matrix, "BILINEAR")
+				after_transform = tf.contrib.image.transform(_feature_map,
+															 matrix,
+															 "BILINEAR")
 				# after_transforms.append(after_transform)
 				after_transform = tf.expand_dims(after_transform, 0)
-				# roi = tf.image.crop_and_resize(after_transform, [[0, 0, 8/720.0, width_box/1280.0]], [0], [8, width_box])
+				# roi = tf.image.crop_and_resize(after_transform,
+				#								 [[0, 0, 8/720.0,
+				#								  width_box/1280.0]],
+				#								 [0],
+				#								 [8, width_box])
 
-				# roi = tf.image.crop_and_resize(after_transform, [[0, 0, 8/(config.INPUT_IMAGE_SIZE / 4.0), width_box/(config.INPUT_IMAGE_SIZE / 4.0)]], [0], [8, width_box])
+				# roi = tf.image.crop_and_resize(after_transform,
+				#								 [[0,
+				#								   0,
+				#								   8/(config.INPUT_IMAGE_SIZE / 4.0),
+				#								   width_box/(config.INPUT_IMAGE_SIZE / 4.0)]],
+				#								  [0],
+				#								  [8, width_box])
 				# There are some erros
-				# roi = tf.image.crop_and_resize(after_transform, [[0, 0, 8/(config.INPUT_SIZE / 4.0), width_box/(config.INPUT_SIZE / 4.0)]], [0], [8, tf.cast(width_box, tf.int32)])
-				roi = tf.image.crop_and_resize(after_transform, [[0, 0, 8/map_shape[0], width_box/map_shape[1]]], [0], [8, tf.cast(width_box, tf.int32)])
-				#  = tf.image.crop_and_resize(after_transform, [[0, 0, 8/config.INPUT_IMAGE_SIZE, width_box/config.INPUT_IMAGE_SIZE]], [0], [8, width_box])
-				# roi = tf.image.crop_and_resize(after_transform, [[0, 0, 8/128.0, width_box/128.0]], [0], [8, width_box])
+				# roi = tf.image.crop_and_resize(after_transform,
+				#								 [[0,
+				#								   0,
+				#								   8/(config.INPUT_SIZE / 4.0),
+				#								  width_box/(config.INPUT_SIZE / 4.0)]],
+				#								 [0],
+				#								 [8,
+				#								  tf.cast(width_box, tf.int32)])
+				roi = tf.image.crop_and_resize(after_transform,
+											   [[0, 0, 8/map_shape[0],
+											    width_box/map_shape[1]]],
+											   [0],
+											   [8,
+											    tf.cast(width_box,
+														tf.int32)])
+				#  = tf.image.crop_and_resize(after_transform,
+				#							  [[0,
+				#							  	0,
+				#								8/config.INPUT_IMAGE_SIZE,
+				#								width_box/config.INPUT_IMAGE_SIZE]],
+				#							  [0],
+				#							  [8, width_box])
+				# roi = tf.image.crop_and_resize(after_transform,
+				#								 [[0,
+				#								   0,
+				#								   8/128.0,
+				#								   width_box/128.0]],
+				#								 [0],
+				#								 [8, width_box])
 				pad_roi = tf.image.pad_to_bounding_box(roi, 0, 0, 8, max_width)
 				pad_rois = pad_rois.write(i, pad_roi)
 				i += 1
@@ -205,7 +281,10 @@ def dummy_input():
 
 			x1, y1, x2, y2, x3, y3, x4, y4 = map(eval, info[:8])
 
-			rotated_rect = cv2.minAreaRect(np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]]))
+			rotated_rect = cv2.minAreaRect(np.array([[x1, y1],
+													 [x2, y2],
+													 [x3, y3],
+													 [x4, y4]]))
 			box_w, box_h = rotated_rect[1][0], rotated_rect[1][1]
 
 			map_w = img.shape[1]
@@ -225,12 +304,19 @@ def dummy_input():
 
 			# src_pts = np.float32([(x1, y1), (x2, y2),(x3, y3), (x4, y4)])
 			src_pts = np.float32([(x1, y1), (x2, y2), (x4, y4)])
-			# dst_pts = np.float32([(mapped_x1, mapped_y1), (mapped_x2, mapped_y2), (mapped_x3, mapped_y3), (mapped_x4, mapped_y4)])
-			dst_pts = np.float32([(mapped_x1, mapped_y1), (mapped_x2, mapped_y2), (mapped_x4, mapped_y4)])
+			# dst_pts = np.float32([(mapped_x1, mapped_y1),
+			#						(mapped_x2, mapped_y2),
+			#						(mapped_x3, mapped_y3),
+			#						(mapped_x4, mapped_y4)])
+			dst_pts = np.float32([(mapped_x1, mapped_y1),
+								  (mapped_x2, mapped_y2),
+								  (mapped_x4, mapped_y4)])
 
-			affine_matrix = cv2.getAffineTransform(dst_pts.astype(np.float32), src_pts.astype(np.float32))
+			affine_matrix = cv2.getAffineTransform(dst_pts.astype(np.float32),
+												   src_pts.astype(np.float32))
 			affine_matrix = affine_matrix.flatten()
-			# project_matrix = cv2.getPerspectiveTransform(dst_pts.astype(np.float32), src_pts.astype(np.float32))
+			# project_matrix = cv2.getPerspectiveTransform(dst_pts.astype(np.float32),
+			#											   src_pts.astype(np.float32))
 			# project_matrix = project_matrix.flatten()[:8]
 
 			box_widths.append(width_box)
@@ -260,8 +346,14 @@ def check_RoIRotate(RR):
 	for i in range(2): # Batch size is 2
 		input_box_masks.append(tf.placeholder(tf.int32, shape=[None]))
 
-	# pad_rois = RR.roi_rotate_tensor(input_feature_map, input_transform_matrix, input_box_masks, input_box_widths)
-	pad_rois = RR.roi_rotate_tensor_pad(input_feature_map, input_transform_matrix, input_box_masks, input_box_widths)
+	# pad_rois = RR.roi_rotate_tensor(input_feature_map,
+	#								  input_transform_matrix,
+	#								  input_box_masks,
+	#								  input_box_widths)
+	pad_rois = RR.roi_rotate_tensor_pad(input_feature_map,
+										input_transform_matrix,
+										input_box_masks,
+										input_box_widths)
 
 	data = dummy_input()
 	for i in range(6):
@@ -269,7 +361,11 @@ def check_RoIRotate(RR):
 			print(data[i].shape)
 
 	with tf.Session() as sess:
-		inp_dict = {input_feature_map: data[0], input_feature_height: data[1], input_feature_width: data[2], input_transform_matrix: data[3], input_box_widths: data[5]}
+		inp_dict = {input_feature_map: data[0],
+					input_feature_height: data[1],
+					input_feature_width: data[2],
+					input_transform_matrix: data[3],
+					input_box_widths: data[5]}
 		for i in range(2):
 			inp_dict[input_box_masks[i]] = data[4][i]
 		result_rois = sess.run(pad_rois, feed_dict=inp_dict)
